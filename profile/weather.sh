aaaf49e29aa4d2121e8eeff3ca7cadcbd17f98cd
gfigueir <gfigueir@redhat.com>
2017-03-12 06:31:24 +0100

    fix for weather.sh

--- profile/weather.sh
+++ profile/weather.sh
@@ -1,6 +1,6 @@
 if test "`find /tmp/weather -mmin +30`"
 then
-  curl wttr.in/Brno > /tmp/weather
+  curl wttr.in/Brno |  sed -r "s/\x1B\[(([0-9]+)(;[0-9]+)*)?[m,K,H,f,J]//g" > /tmp/weather
 fi
 
-head -4 /tmp/weather  | tail -2 | cut -b 31-71 | sed 'N;s/\n/ /' | sed -r "s/\x1B\[(([0-9]+)(;[0-9]+)*)?[m,K,H,f,J]//g"
+head -4 /tmp/weather  | tail -2 | cut -b 16-40 | sed 's/ *$//' | sed 'N;s/\n/ /' 
