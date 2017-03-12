dde18bce86b52ceb7f802be1524189c70d63fdf9
gfigueir <gfigueir@redhat.com>
2017-03-12 14:07:18 +0100

    continuous integration article, profile setup

--- profile/install.sh
+++ profile/install.sh
@@ -1,3 +1,20 @@
-cp ./.bashrc ~/.bashrc
-cp ./.vimrc ~/.vimrc
-cp ./.tmux.conf ~/.tmux.conf
+#!/bin/bash
+#
+# setup phase
+sudo zypper install git tmux
+mkdir ~/git && cd ~/git
+git clone https://github.com/guxtavo/wiki.git
+~/git/wiki/profile/install.sh
+
+# copying and sourcing
+cd ~/git/wiki/profile
+cp .bashrc ~/.bashrc && source ~/.bashrc
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+
+# git cloning
+cd ~/git
+git clone https://github.com/guxtavo/aarav.git 
+git clone https://github.com/guxtavo/myscripts.git 
+git clone https://github.com/brendangregg/perf-tools.git
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
