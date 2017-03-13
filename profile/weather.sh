fba42c9a31b612b5ea28523e43ff559b86ce0dca
gfigueir <gfigueir@redhat.com>
2017-03-13 08:50:45 +0100

    small modifications for profile/weather

--- profile/weather.sh
+++ profile/weather.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # feed /tmp/weather from wttr.in and use regex to manipulate data
+# 
 # fix me - if no internet, file could be empty, try a second time maybe or 
 # something more robust
 
@@ -14,12 +15,17 @@ get_weather(){
 
 # main funtion
 main(){
+
+  # fetch data if files is 30 minutes old
   if test "`find /tmp/weather -mmin +30`"
   then
     get_weather
   fi
 
-  if test -s /tmp/weather
+  # maybe add some timer here
+
+  # if the file is empty, try again to fetch the data 
+  if test ! -s /tmp/weather
   then
     get_weather
   fi
@@ -42,6 +48,3 @@ case $1 in
     main
     ;;
 esac
-
-
-
