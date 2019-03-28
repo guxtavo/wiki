100d71902f7bfe4e65c0914d2e485c1e47429616
Gustavo Figueira <gfigueira@suse.com>
2019-03-28 20:56:49 +0100

    fixing openbsd skeleton

--- profile/dotfiles/.openbsd.sh
+++ profile/dotfiles/.openbsd.sh
@@ -2,6 +2,8 @@ export CVSROOT=anoncvs@mirror.osn.de:/cvs
 
 export HISTFILE="/home/gfigueira/.ksh_history"
 export HISTSIZE=9999
+export EDITOR=vi
+set -o emacs
 
 alias vpn_on="doas openvpn --daemon --config /etc/openvpn/SUSE.conf.PRG"
 alias vpn_off="doas pkill openvpn"
