08cf8980a0801607ae3c6a1ab5e2735d75dcb0f0
Gustavo Figueira <gfigueira@suse.com>
2019-03-29 14:45:25 +0100

    yet another weather fix for openbsd

--- profile/plugins/st.sh
+++ profile/plugins/st.sh
@@ -289,7 +289,10 @@ weather_format_data()
   if [ $SYSTEM = "Linux" ]; then
   echo -n  $(sed -n 13p /dev/shm/weather | grep -o '[0-9]\{1,2\}' |sort -n | sed -e 1b -e '$!d' | tr '\n' ' ' | awk '{print $1"-"$2"."}')
 	else
-	 echo -n  $(sed -n 13p /dev/shm/weather | strings| grep -o 'm[0-9]\{1,2\}')
+	 RAW=$(sed -n 13p /dev/shm/weather | strings | grep -o 'm[0-9]\{1,2\}')
+	 BETTER=$(echo $RAW | tr -d "m" | sort -n | sed -e 1b -e '$!d')
+	 ENHANCE=$(echo $BETTER | tr '\n' ' ' | awk '{print $1"/"$2"."}') 
+	 echo -n $ENHANCE
 	fi
 }
 
